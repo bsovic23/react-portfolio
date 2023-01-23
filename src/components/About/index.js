66fdad74139f140433eb02061f6654748d71ff97
Brit Sovic <bsovic@tulane.edu>
2023-01-23 14:15:38 -0500

    updated about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,19 +9,22 @@ function About() {
                         alt="picture of Brit Sovic"/>
                 </div>
                 <div>
-                <h2>Fun Facts</h2>
-                <ul>
-                    <li>Favorite Sport: Basketball</li>
-                    <li>Favorite NFL team: Dallas Cowboys</li>
-                    <li>Favorite NBA team: Cleveland Cavaliers</li>
-                </ul>
-                <h2>Education/Occupation</h2>
-                <ul>
-                    <li>Undergrad: Syracuse University, BS - Public Health</li>
-                    <li>Graduate: Tulane University, MPH - Epidemiology</li>
-                    <li>Occupation: Data for Infectious Diseases Department of Medicine
-                        at Columbia University Irving Medical Center</li>
-                </ul>
+                    <h2>Education/Occupation</h2>
+                    <ul>
+                        <li>Undergraduate Degree: Syracuse University, BS - Public Health</li>
+                        <li>Graduate Degree: Tulane University, MPH - Epidemiology</li>
+                        <li>Certificate: </li>
+                        <li>Current Occupation: Data Analysis and Managment for the Infectious Diseases Department of Medicine
+                            at Columbia University Irving Medical Center</li>
+                    </ul>
+                    <h2>Fun Facts</h2>
+                    <ul>
+                        <li>Favorite Sport: Basketball</li>
+                        <li>Favorite NFL team: Dallas Cowboys</li>
+                        <li>Favorite NBA team: Cleveland Cavaliers</li>
+                    </ul>
+                    <h2>Languages</h2>
+                    <li>JavaScript, CSS, HTML</li>
                 </div>
         </section>
     )
