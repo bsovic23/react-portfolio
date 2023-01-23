f5af4c2a0a3b9a01027ebbe97d4ad7401a90ad6a
Brit Sovic <bsovic@tulane.edu>
2023-01-23 15:54:51 -0500

    finished homepage - added description of nav bar tabs

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,42 @@ import React from 'react';
 
 function Home() {
     return(
-        <h1>Welcome to Brits Portfolio</h1>
+        <section class="home">
+            <h1>Welcome to Brits Portfolio</h1>
+            <p>
+                This portfolio includes different sectiosn that display aspects of my coding abilities and information about me.
+            </p>
+
+            <h3>Portfolio Sections</h3>
+            <p>
+                Below are descriptions of which tab you should visit to find the information necessary
+            </p>
+            <ul>
+                <div class="home-tab">
+                    <h4>About</h4>
+                    <li>Education/Occupation</li>
+                    <li>Fun Facts</li>
+                    <li>Languages</li>
+                </div>
+
+                <div class="home-tab">
+                    <h4>Contact</h4>
+                    <li>Form to fill out to contact me</li>
+                </div>
+
+                <div class="home-tab">
+                    <h4>Portfolio</h4>
+                    <li>Group Projects</li>
+                    <li>Self-Created Projects</li>
+                    <li>Challenge Assignments</li>
+                </div>
+
+                <div class="home-tab">
+                    <h4>Resume</h4>
+                    <li>Download a copy of my resume</li>
+                </div>
+            </ul>
+        </section>
     )
 };
 
